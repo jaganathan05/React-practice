ba137f7e9f9a5a7d36e117ea1212038b0ded1745
jaganathan05 <jaganathanv888@gmail.com>
2024-03-23 14:05:24 +0530

    Refs and Controlled Components

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState ,useRef} from 'react';
 import Wrapper from '../Helpers/Wrapper';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,13 +6,20 @@ import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef()
+  const ageInputRef = useRef()
+  const collageInputref = useRef()
+
+  
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+
+    const enteredUsername = nameInputRef.current.value
+    const enteredAge = ageInputRef.current.value
+    const entercollagename = collageInputref.current.value
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0 || entercollagename.trim().length===0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
@@ -26,18 +33,12 @@ const AddUser = (props) => {
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
+    props.onAddUser(enteredUsername, enteredAge , entercollagename) ;
+    nameInputRef.current.value=''
+    ageInputRef.current.value=''
+    collageInputref.current.value=''
   };
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
 
   const errorHandler = () => {
     setError(null);
@@ -58,16 +59,18 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
+          <label htmlFor='collagename'>collage name</label>
+          <input id='collagename'
+          type='text'
+          ref={collageInputref} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
